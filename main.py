20c19f8b88dac825999559366b65a0f6309a55cf
Matt Green <verde.mateo.a@gmail.com>
2020-11-22 16:54:14 -0500

    [Start-Up]: Can Terminate

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 """
  Tic Tac Toe
 """
-print('Hello World')
+
+
+def ticTacToe():
+    running = True
+    yesResponses = ['Y', 'Yes', 'Si']
+    while running:
+        quit = input("Do you want to quit?:  ")
+        print(f'{quit}')
+        if quit in yesResponses:
+            running = False
+
+
+def printHW():
+    print('Hello World')
+
+
+if __name__ == '__main__':
+    ticTacToe()
