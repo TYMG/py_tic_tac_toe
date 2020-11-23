97ba699f7c4b5dde872947ef580f945f9412f123
Matt Green <verde.mateo.a@gmail.com>
2020-11-22 22:02:01 -0500

    [Print Board]: Print With Input

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ shut_down_responses = {'quit': 'quit', 'exit': 'exit', 'restart': 'restart'}
 player_1 = 'Player 1'
 player_2 = 'Player 2'
 
-seperator = '------------'
 empty_row = [' ', ' ', ' ']
 
 
@@ -36,7 +35,7 @@ def tic_tac_toe():
                     break
                 else:
                     print('Sorry that is not a valid input.\nPlease enter X or O\n')
-            result = game_on(users)
+            result = game(users)
             if result == shut_down_responses['exit']:
                 running = False
             else:
@@ -62,13 +61,61 @@ def set_player_symbol(users, symbol):
     return users
 
 
-def game_on(users):
+def game(users):
     global shut_down_responses
+    global player_1
+    global player_2
 
-    game_board = ['', '', '']*3
+    game_on = True
+    player_1_turn = True
+    game_board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
     print('\nStarting Game' + '...'*4)
-    print(game_board)
+    print_board(game_board)
+    while game_on:
+        is_up = ' is up'
+        symbol = None
+        if player_1_turn:
+            print(f"{player_1}"+is_up)
+            symbol = users[player_1]
+        elif player_1_turn == False:
+            print(f"{player_2}"+is_up)
+            symbol = users[player_2]
+        row = ''
+        col = ''
+        while row.isdigit() == False:
+            row = input('Please enter a Row (1,2,3): ')
+            if(row.isdigit() == False):
+                print('Please enter a numeric value (1,2,3)')
+        row = int(row)
+        while col.isdigit() == False:
+            col = input('Please enter a Col (1,2,3): ')
+            if(col.isdigit() == False):
+                print('Please enter a numeric value (1,2,3)')
+        col = int(row)
+        update_board(game_board, symbol, row-1, col-1)
+        print_board(game_board)
+        player_1_turn = not player_1_turn
+        game_on = False
     return shut_down_responses['exit']
+
+
+def update_board(game_board, symbol, row, col):
+    game_board[row][col] = symbol
+
+
+def print_board(game_board):
+
+    seperator = '-------------'
+    print('\n'+seperator)
+    for values in game_board:
+        print('| {0:2}| {1:^2}| {2:2}|'.format(
+            values[0], values[1], values[2]))
+        print(seperator)
+    print('\n')
+
+
+def print_winner(victor):
+    print(f'{victor} is the winner!!!')
 
 
 if __name__ == '__main__':
